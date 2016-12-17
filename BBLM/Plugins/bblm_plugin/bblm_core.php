de4273ec23f4a144d884925e5a33806ae6c2da49
blacksnotling <andrew@Andrews-Mac-mini.local>
2016-12-17 18:32:51 +0000

    Version bump to 1.1.1
    originally 1.6.1 but never mind

--- BBLM/Plugins/bblm_plugin/bblm_core.php
+++ BBLM/Plugins/bblm_plugin/bblm_core.php
@@ -3,7 +3,7 @@
 Plugin Name: Blood Bowl League Manager System (BBLM)
 Plugin URI: http://www.hdwsbbl.co.uk/
 Description: Everthing you need to run a Blood Bowl League via Wordpress!
-Version: 1.6
+Version: 1.1.1
 Author: Blacksnotling
 Author URI: http://www.hdwsbbl.co.uk/
 */
