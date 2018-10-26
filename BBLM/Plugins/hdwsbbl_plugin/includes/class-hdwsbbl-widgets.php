33582574300b17ce39ea68e06f69a7e14af5ef93
blacksnotling <Black.snotling@gmail.com>
2018-10-26 18:14:39 +0100

    Initial commit of the HDWSBBL Plugin!
    For unique stuff only needed for the HDWSBBL (things like Warzone etc)

---
+++ BBLM/Plugins/hdwsbbl_plugin/includes/class-hdwsbbl-widgets.php
@@ -0,0 +1,18 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/**
+ * Import the Widgets used in the plugin
+ *
+ * @author 		Blacksnotling
+ * @category 	Admin
+ * @package 	HDWSBBL/Widgets
+ * @version   1.0
+ */
+
+
+/**
+ * Loads all the Widgets used in the league
+ */
