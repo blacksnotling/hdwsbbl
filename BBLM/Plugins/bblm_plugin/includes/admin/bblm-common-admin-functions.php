94b177c13f0af54c30109bff113c799e4654d378
blacksnotling <Black.snotling@gmail.com>
2018-10-26 14:42:38 +0100

    Moved the core of the freebooter (JM) report to a common file
    I can now display this on the dashboard in a future commit rather than
    duplicating code.

--- BBLM/Plugins/bblm_plugin/includes/admin/bblm-common-admin-functions.php
+++ BBLM/Plugins/bblm_plugin/includes/admin/bblm-common-admin-functions.php
@@ -63,3 +63,44 @@ function bblm_update_player($pid, $counts = 1) {
 	}
 	return true;
 }
+
+ /**
+  * Displays the Journeyman / Mercenary output
+  * Lists of teams and lists of output
+  */
+function bblm_jm_report() {
+  global $wpdb;
+
+  //call the options from the table
+	$options = get_option('bblm_config');
+	$merc_pos = htmlspecialchars($options['player_merc'], ENT_QUOTES);
+
+	$jmsql = 'SELECT P.post_title AS Player, O.post_title AS Team, X.p_num, Z.pos_name, X.p_id FROM '.$wpdb->prefix.'player X, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' P, '.$wpdb->prefix.'bb2wp I, '.$wpdb->posts.' O, '.$wpdb->prefix.'position Z WHERE X.pos_id = Z.pos_id AND X.p_id = J.tid AND J.prefix = \'p_\' AND J.pid = P.ID AND X.t_id = I.tid AND I.prefix = \'t_\' AND I.pid = O.ID AND X.p_status = 1 AND (X.pos_id = 1 OR X.pos_id = '.$merc_pos.') ORDER BY X.t_id, X.p_num';
+
+	if ( $journeymen = $wpdb->get_results($jmsql) ) {
+		$is_first = 1;
+		$current_team = "";
+
+		foreach ($journeymen as $jm) {
+			if ($jm->Team !== $current_team) {
+				$current_team = $jm->Team;
+				if (1 !== $is_first) {
+					print(" </ul>\n");
+				}
+				$is_first = 1;
+			}
+			if ($is_first) {
+				print("<h3>".$jm->Team."</h3>\n <ul>\n");
+				$is_first = 0;
+			}
+			print ("   <li># ".$jm->p_num." - ".$jm->Player." (<em>".$jm->pos_name."</em>) -- <<a href=\"");
+			bloginfo('url');
+			print("/wp-admin/admin.php?page=bblm_plugin/pages/bb.admin.edit.player.php&action=edit&item=remove&id=".$jm->p_id."\" title=\"Remove this freebooter from the team\">Retire / Remove</a>></li>\n");
+		}
+		print("</ul>\n");
+	}
+	else {
+		print("<p><strong>There are no Journeymen or Mercs currently active in the league!</strong></p>\n");
+	}
+
+}
