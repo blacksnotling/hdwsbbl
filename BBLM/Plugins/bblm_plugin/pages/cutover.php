769f71d5fada6a7eb13c9bd3a93c9507a8ecf5c1
blacksnotling <Black.snotling@gmail.com>
2018-11-03 14:04:09 +0000

    Updating Cutover page for Owners

--- BBLM/Plugins/bblm_plugin/pages/cutover.php
+++ BBLM/Plugins/bblm_plugin/pages/cutover.php
@@ -59,6 +59,62 @@ if (isset($_POST['bblm_dyk_convert'])) {
   } // end of if DYK posts exist
 
 } // END OF ONVERTING DYK ENTIRES INTO CPT
+/**
+ *
+ * CONVERTING OWNER ENTIRES INTO CPT
+ */
+if (isset($_POST['bblm_owner_convert'])) {
+
+	//First we grab a list of the current users
+	$ownersql = "SELECT ID, display_name FROM `".$wpdb->prefix."users`";
+	//We check something was returned
+	if ($ownerlist = $wpdb->get_results($ownersql)) {
+
+		//echo '<ul>';
+		//Then we loop through them
+		foreach ($ownerlist as $owl) {
+			//We add the new owner to the database
+			//echo '<li>'.$owl->ID.' - '.$owl->display_name.'</li>';
+			$post_id = wp_insert_post( array( 'post_title'=>$owl->display_name, 'post_type'=>'bblm_owner', 'post_content'=>'', 'post_status'=>'publish' ) );
+
+			if( !is_wp_error( $post_id ) ){
+
+				//Then use that ID to update the teams table
+				$oupdatesql = "UPDATE  `".$wpdb->prefix."team` SET  `ID` =  '".$post_id."' WHERE  `".$wpdb->prefix."team`.`ID` = ".$owl->ID;
+
+				if ( $wpdb->query($oupdatesql) ) {
+					$result = true;
+				}
+				else {
+
+					//Updating the team table failed!
+					$result = false;
+
+				}
+
+
+				echo '<ul><li>'.$oupdatesql.'</li></ul>';
+
+				$result = true;
+
+			}
+			else {
+
+				//failed to insert post
+				$result = false;
+
+			}
+
+
+		}//end of foreach
+		//echo '</ul>';
+	}//end of if sql was correct
+	if ( $result ) {
+		print("<div id=\"updated\" class=\"updated fade\"><p>Owners have been converted!</p></div>\n");
+	}
+
+
+} // END OF ONVERTING OWNER ENTIRES INTO CPT
 
 /**
  *
@@ -68,6 +124,8 @@ if (isset($_POST['bblm_dyk_convert'])) {
 <p>This screen should only be used when performing the cutover. Use each option <strong>only once</strong>.</p>
 
 <form name="bblm_cutovermain" method="post" id="post">
+
+	<h2>1.x -> 1.7</h2>
   <h3>General</h3>
   <ul>
 		<li>Create a new page called "front page" (if it does not already exist)</li>
@@ -88,6 +146,22 @@ if (isset($_POST['bblm_dyk_convert'])) {
     <li>Delete the &quot;Did You Know&quot; Page. - make sure you add it back into the Menu</li>
     <li>You can now delete the DYK table if you wish!</li>
   </ul>
+
+	<h2>1.7 -> 1.8</h2>
+	<h3>General</h3>
+	<ul>
+		<li>Update the settings page, with the text that goes at the top of the Owners / Coaches page</li>
+		<li>Double check the teams assigned to users. The star players team (52) should be assigned to user 1, and Mycenaen Marauders (83) should be assigned to the correct coach</li>
+	</ul>
+
+	<h3>Owners (NEW)</h3>
+	<ul>
+		<li><input type="submit" name="bblm_owner_convert" value="Convert Owners" title="Convert the Owners"/></li>
+		<li>Change Player 1 (Might be admin or similar) to THE HDWSBBL or something similar and set the visibility to private</li>
+		<li>Clean up the names as required</li>
+		<li>Any teams that need to be deleted? t_show = 0</li>
+		<li>Doubloe check all the teams are assigne dcorrectly-  no orphins etc</li>
+	</ul>
 
 </form>
 
