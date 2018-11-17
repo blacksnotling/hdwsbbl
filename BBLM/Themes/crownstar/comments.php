763b25caf91119d548f4b26a40b2dca563ce4df7
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2018-11-17 23:52:49 +0000

    Updated Comment Wording

--- BBLM/Themes/crownstar/comments.php
+++ BBLM/Themes/crownstar/comments.php
@@ -32,13 +32,13 @@ if ( post_password_required() ) {
 			if ( '1' === $crownstar_comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'crownstar' ),
+					esc_html__( 'One response to &ldquo;%1$s&rdquo;', 'crownstar' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $crownstar_comment_count, 'comments title', 'crownstar' ) ),
+					esc_html( _nx( '%1$s response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', $crownstar_comment_count, 'comments title', 'crownstar' ) ),
 					number_format_i18n( $crownstar_comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
