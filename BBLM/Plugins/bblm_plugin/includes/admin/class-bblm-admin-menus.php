cfb3d0034640389aefd74ede45d27ee3de693a1f
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2019-06-10 21:41:48 +0100

    Version Bump
    1.8 is done!

--- BBLM/Plugins/bblm_plugin/includes/admin/class-bblm-admin-menus.php
+++ BBLM/Plugins/bblm_plugin/includes/admin/class-bblm-admin-menus.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @author 		Blacksnotling
  * @category 	Admin
  * @package 	BBowlLeagueMan/Admin
- * @version   1.0
+ * @version   1.1
  */
 
 if ( ! class_exists( 'BBLM_Admin_Menus' ) ) :
