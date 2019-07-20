806b4595bdbadee3d563609d6fbc41ffe956709e
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2019-07-20 18:10:41 +0100

    Updated Add Player Match for exhibition games
    Phew

--- BBLM/Plugins/bblm_plugin/pages/bb.admin.add.match_player.php
+++ BBLM/Plugins/bblm_plugin/pages/bb.admin.add.match_player.php
@@ -34,8 +34,8 @@ if (isset($_POST['bblm_player_increase'])) {
 while ($p <= $pmax){
 
 	//if  "on" result in "changed" then generate SQL
-	if (on == $_POST['bblm_pcng'.$p]) {
-
+	if ( isset( $_POST['bblm_pcng'.$p] ) ) {
+		if ( "on" == $_POST['bblm_pcng'.$p] ) {
 
 		$updatesql = 'UPDATE `'.$wpdb->prefix.'player` SET `p_ma` = \''.$_POST['bblm_pma'.$p].'\', `p_st` = \''.$_POST['bblm_pst'.$p].'\', `p_ag` = \''.$_POST['bblm_pag'.$p].'\', `p_av` = \''.$_POST['bblm_pav'.$p].'\', `p_skills` = \''.$_POST['bblm_pskills'.$p].'\', `p_injuries` = \''.$_POST['bblm_pinjuries'.$p].'\', `p_cost` = \''.$_POST['bblm_pcost'.$p].'\'';
 
@@ -45,9 +45,10 @@ while ($p <= $pmax){
 
 		$updatesql .= ' WHERE `p_id` = '.$_POST['bblm_pid'.$p].' LIMIT 1';
 
-	$playersqla[$p] = $updatesql;
+		$playersqla[$p] = $updatesql;
 
-	} //end of if changed
+		} //end of if changed
+	}
 
 	$p++;
 
@@ -75,13 +76,7 @@ foreach ($playersqla as $ps) {
 		$sucess = TRUE;
 	}
 }
-/*
-if (FALSE !== $wpdb->query($updateteamAsql)) {
-	$sucess = TRUE;
-}
-if (FALSE !== $wpdb->query($updateteamBsql)) {
-	$sucess = TRUE;
-}*/
+
 bblm_update_tv($_POST['bblm_teamA']);
 bblm_update_tv($_POST['bblm_teamB']);
 if (FALSE !== $wpdb->query($updatematchsql)) {
@@ -96,11 +91,6 @@ if ($sucess) {
  //  !!END OF PROCESS!!  //
 //////////////////////////
 
-
-
-
-
-
 }
 else if (isset($_POST['bblm_player_actions'])) {
 	//3rd Step: Recording the players actions for the match
@@ -112,6 +102,7 @@ else if (isset($_POST['bblm_player_actions'])) {
 	print("</pre>\n<hr>\n");*/
 
 	$compcounts = $_POST['bblm_ccounts'];
+	$finished = 0;
 
 
 $playermatchsql = "INSERT INTO `".$wpdb->prefix."match_player` (`m_id`, `p_id`, `t_id`, `mp_td`, `mp_cas`, `mp_comp`, `mp_int`, `mp_mvp`, `mp_spp`, `mp_mng`, `mp_inj`, `mp_inc`, `mp_counts`) VALUES ";
@@ -124,26 +115,25 @@ $playermatchsql = "INSERT INTO `".$wpdb->prefix."match_player` (`m_id`, `p_id`, 
 	$reactivatesql = array();
 
 	//before we begin the main loop, we re-activate all the players who missed the game
-	if ($compcounts)  {
-		$selectinjplayer = 'SELECT p_id FROM '.$wpdb->prefix.'player WHERE p_mng = 1 AND (t_id = '.$_POST['bblm_teamA'].' or t_id = '.$_POST['bblm_teamB'].')';
+	$selectinjplayer = 'SELECT p_id FROM '.$wpdb->prefix.'player WHERE p_mng = 1 AND (t_id = '.$_POST['bblm_teamA'].' or t_id = '.$_POST['bblm_teamB'].')';
 
-		if ($injplayer = $wpdb->get_results($selectinjplayer)) {
-			foreach ($injplayer as $ip) {
-				$reactivatesql[] .= 'UPDATE `'.$wpdb->prefix.'player` SET `p_mng` = \'0\', `p_cost_ng` = p_cost  WHERE `p_id` = '.$ip->p_id.' LIMIT 1';
-			}
+	if ($injplayer = $wpdb->get_results($selectinjplayer)) {
+		foreach ($injplayer as $ip) {
+			$reactivatesql[] .= 'UPDATE `'.$wpdb->prefix.'player` SET `p_mng` = \'0\', `p_cost_ng` = p_cost  WHERE `p_id` = '.$ip->p_id.' LIMIT 1';
 		}
-
-	} //end of compcounts
+	}
 
 
 //Initialize var to capture first input
 $is_first_player = 1;
+$playersqla = array();
 
 //Beginning of main loop.
 while ($p <= $pmax){
 
 //before we go any further, we should see if the player in question ctually took part in the match!
-if (on == $_POST['bblm_plyd'.$p]) {
+if ( isset( $_POST['bblm_plyd'.$p] ) ) {
+	if ("on" == $_POST['bblm_plyd'.$p]) {
 
 
 	//we only want a comma added for all but the first
@@ -151,10 +141,21 @@ if (on == $_POST['bblm_plyd'.$p]) {
 		$playermatchsql .= ", ";
 	}
 
+	$mng = array();
 	//Set the default "on" result from a checkbox to a 1
-	if (on == $_POST['mng'.$p]) {
-		$_POST['mng'.$p] = 1;
+	if ( isset( $_POST['mng'.$p] ) ) {
+		$mng[$p] = $_POST['mng'.$p];
+		if ( "on" == $mng[$p] ) {
+			$mng[$p] = 1;
+		}
+		else {
+			$mng[$p] = 0;
+		}
 	}
+	else {
+		$mng[$p] = 0;
+	}
+
 	//Fill in blanks for Injuries and Increases
 	if (empty($_POST['bblm_injury'.$p])) {
 		$_POST['bblm_injury'.$p] = "none";
@@ -164,25 +165,35 @@ if (on == $_POST['bblm_plyd'.$p]) {
 	}
 
 	//generate the SQL
-	$playermatchsql .= '(\''.$_POST['bblm_mid'].'\', \''.$_POST['bblm_pid'.$p].'\', \''.$_POST['bblm_tid'.$p].'\', \''.$_POST['bblm_td'.$p].'\', \''.$_POST['bblm_cas'.$p].'\', \''.$_POST['bblm_comp'.$p].'\', \''.$_POST['bblm_int'.$p].'\', \''.$_POST['bblm_mvp'.$p].'\', \''.$_POST['bblm_spp'.$p].'\', \''.$_POST['mng'.$p].'\', \''.$_POST['bblm_injury'.$p].'\', \''.$_POST['bblm_increase'.$p].'\', \'1\')';
+	$playermatchsql .= '(\''.$_POST['bblm_mid'].'\', \''.$_POST['bblm_pid'.$p].'\', \''.$_POST['bblm_tid'.$p].'\', \''.$_POST['bblm_td'.$p].'\', \''.$_POST['bblm_cas'.$p].'\', \''.$_POST['bblm_comp'.$p].'\', \''.$_POST['bblm_int'.$p].'\', \''.$_POST['bblm_mvp'.$p].'\', \''.$_POST['bblm_spp'.$p].'\', \''. $mng[$p] .'\', \''.$_POST['bblm_injury'.$p].'\', \''.$_POST['bblm_increase'.$p].'\', \''.$compcounts.'\')';
 
 	//if the comp counts, update the bb_player table with new spp values and p_mng if player is injured.
+	$playerupdatesql = "";
 	if ($compcounts)  {
-		$playerupdatesql = 'UPDATE `'.$wpdb->prefix.'player` SET `p_spp` = p_spp+\''.$_POST['bblm_spp'.$p].'\'';
-			if ($_POST['mng'.$p])  {
-				$playerupdatesql .= ', `p_cost_ng` = \'0\', `p_mng` = \'1\'';
-			}
+		$playerupdatesql = "UPDATE ".$wpdb->prefix."player SET `p_spp` = p_spp+." . $_POST['bblm_spp'.$p];
+		if ( $mng[$p] )  {
+			$playerupdatesql .= ', `p_cost_ng` = \'0\', `p_mng` = \'1\'';
+		}
 		$playerupdatesql .= ' WHERE `p_id` = \''.$_POST['bblm_pid'.$p].'\' LIMIT 1';
-		//once we have the sql generated, we can insert into the array to insert later on
-		$playersqla[$p] = $playerupdatesql;
-
-		//change flag so that comma's are added to the insert sql string.
-		//We do this here because we don't want the flag to change until someone has been entered into the db
-	$is_first_player = 0;
-
+	}
+	else {
+		//If the game is an exhibition game
+		if ( $mng[$p] )  {
+			$playerupdatesql = 'UPDATE `'.$wpdb->prefix.'player` SET `p_cost_ng` = \'0\', `p_mng` = \'1\' WHERE `p_id` = \''.$_POST['bblm_pid'.$p].'\' LIMIT 1';
+		}
 	}
 
+	//once we have the sql generated, we can insert into the array to insert later on
+	if ( strlen( $playerupdatesql ) > 0 ) {
+		$playersqla[$p] = $playerupdatesql;
+	}
+
+	//change flag so that comma's are added to the insert sql string.
+	//We do this here because we don't want the flag to change until someone has been entered into the db
+	$is_first_player = 0;
+
 }//end of bblm_plyd checking
+}
 	// increment player and go onto the next one.
 	$p++;
 
@@ -199,7 +210,7 @@ if (FALSE !== $wpdb->query($playermatchsql)) {
 	do_action( 'bblm_post_submission' );
 }
 //then if the comp counts, reset the injured players to active and then update the partivipating players.
-if ($compcounts)  {
+
 	foreach ($reactivatesql as $rs) {
 		if (FALSE !== $wpdb->query($rs)) {
 			$sucess = TRUE;
@@ -210,15 +221,7 @@ if ($compcounts)  {
 			$sucess = TRUE;
 		}
 	}
-}
-else {
-	//If the comp doesn't count the then update the match table to show it is complete and exit.
-	if (FALSE !== $wpdb->query($updatematchsql)) {
-		$sucess = TRUE;
-		$finished = 1;
-		do_action( 'bblm_post_submission' );
-	}
-}
+
 
 
 // For Debug purposes only!
@@ -248,7 +251,7 @@ else {
 	<input type="hidden" name="bblm_teamB" size="3" value="<?php print($_POST['bblm_teamB']); ?>">
 
 	<h3>Record Changes to Players</h3>
-	<p>Below are all the players who took part in the match. If they had an increase, please ensure that the "Chnged?" box is ticked. The Skills and Injuries boxs should be automatically filled but you will have to update any changes to Stats manually.</p>
+	<p>Below are all the players who took part in the match. If they had an increase, please ensure that the "Changed?" box is ticked. The Skills and Injuries boxs should be automatically filled but you will have to update any changes to Stats manually.</p>
 
 <?php
 $playersql = 'SELECT P.*, M.mp_inj, M.mp_inc, T.t_name FROM '.$wpdb->prefix.'match_player M, '.$wpdb->prefix.'player P, '.$wpdb->prefix.'team T WHERE P.p_id = M.p_id AND m_id = '.$_POST['bblm_mid'].' AND M.t_id = T.t_id ORDER BY P.t_id, P.p_num';
@@ -279,6 +282,8 @@ $playersql = 'SELECT P.*, M.mp_inj, M.mp_inc, T.t_name FROM '.$wpdb->prefix.'mat
 				print("  <tr class=\"alternate\">\n");
 			}
 
+			$incmade = 0;
+			$injmade = 0;
 			//Determine if player has had an increase or injury
 			if ("none" !== $pl->mp_inc) {
 				$incmade = 1;
@@ -369,7 +374,7 @@ else if (isset($_POST['bblm_match_select'])) {
 	 // Step 2: Generate a list of payers based on the match selected //
 	///////////////////////////////////////////////////////////////////
 
-$matchsql2 = "SELECT M.m_id, UNIX_TIMESTAMP(M.m_date), M.m_teamA AS tAid, M.m_teamB AS tBid, T.t_name AS tA, Q.t_name AS tB, M.m_teamAtd, M.m_teamBtd, A.mt_cas AS tAcas, B.mt_cas AS tBcas, A.mt_int AS tAint, B.mt_int AS tBint, A.mt_comp AS tAcomp, B.mt_comp AS tBcomp, C.c_counts FROM ".$wpdb->prefix."match M, ".$wpdb->prefix."team T, ".$wpdb->prefix."team Q, ".$wpdb->prefix."comp C, ".$wpdb->prefix."match_team A, ".$wpdb->prefix."match_team B WHERE C.c_id = M.c_id AND M.m_teamA = T.t_id AND M.m_teamB = Q.t_id AND M.m_complete = 0 AND A.m_id = M.m_id AND A.t_id = M.m_teamA AND B.m_id = M.m_id AND B.t_id = M.m_teamB AND M.m_id = ".$_POST['bblm_mid'];
+$matchsql2 = "SELECT M.m_id, UNIX_TIMESTAMP(M.m_date) AS MDATE, M.m_teamA AS tAid, M.m_teamB AS tBid, T.t_name AS tA, Q.t_name AS tB, M.m_teamAtd, M.m_teamBtd, A.mt_cas AS tAcas, B.mt_cas AS tBcas, A.mt_int AS tAint, B.mt_int AS tBint, A.mt_comp AS tAcomp, B.mt_comp AS tBcomp, C.c_counts FROM ".$wpdb->prefix."match M, ".$wpdb->prefix."team T, ".$wpdb->prefix."team Q, ".$wpdb->prefix."comp C, ".$wpdb->prefix."match_team A, ".$wpdb->prefix."match_team B WHERE C.c_id = M.c_id AND M.m_teamA = T.t_id AND M.m_teamB = Q.t_id AND M.m_complete = 0 AND A.m_id = M.m_id AND A.t_id = M.m_teamA AND B.m_id = M.m_id AND B.t_id = M.m_teamB AND M.m_id = ".$_POST['bblm_mid'];
 	if ($md = $wpdb->get_row($matchsql2)) {
 ?>
 	<h3>Match Reference</h3>
@@ -381,7 +386,7 @@ $matchsql2 = "SELECT M.m_id, UNIX_TIMESTAMP(M.m_date), M.m_teamA AS tAid, M.m_te
 			<th scope="col"><?php print($md->tB); ?></th>
 		</thead
 		<tr>
-			<th colspan="3">Date: <?php print(date("d.m.Y", $md->m_date)); ?></th>
+			<th colspan="3">Date: <?php print( date( "d.m.Y", $md->MDATE ) ); ?></th>
 		</tr>
 		<tr class="alternate">
 			<td><?php print($md->m_teamAtd); ?></td>
@@ -423,7 +428,11 @@ $matchsql2 = "SELECT M.m_id, UNIX_TIMESTAMP(M.m_date), M.m_teamA AS tAid, M.m_te
 	<p>Below are all the players who where available to take part in this match. If they took part in this match please ensure that the &quot;Played?&quot; tickbox is selected.</p>
 	<p>There will be a chance to record the actions of any Star Players at the bottom of the page.</p>
 
+<?php
+ 	if ( $ccounts ) {
+		//Only display the Javacript if the match is no an exhibition game
 
+?>
 	<script type="text/javascript">
 	function UpdateSPP(theId) {
 		/*		Calcuate the players SPP		*/
@@ -467,8 +476,10 @@ $matchsql2 = "SELECT M.m_id, UNIX_TIMESTAMP(M.m_date), M.m_teamA AS tAid, M.m_te
 
 	}
 	</script>
-
 	<?php
+	} //end of 'if the competition counts'
+
+	$noplayers = 0;
 $playersql = 'SELECT P.p_id, P.t_id, P.p_spp, X.post_title AS p_name, P.p_num, T.t_name from '.$wpdb->prefix.'player P, '.$wpdb->prefix.'team T, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' X where P.p_id = J.tid AND J.prefix = \'p_\' AND J.pid = X.ID AND P.t_id = T.t_id AND (P.t_id = '.$tAid.' OR P.t_id = '.$tBid.') AND P.p_status = 1 AND P.p_mng = 0 ORDER BY P.t_id, P.p_num';
 	if ($playerlist = $wpdb->get_results($playersql)) {
 		//initiate var for count
@@ -502,10 +513,22 @@ $playersql = 'SELECT P.p_id, P.t_id, P.p_spp, X.post_title AS p_name, P.p_num, T
 			print("   <td><input type=\"text\" name=\"bblm_cas".$p."\" id=\"bblm_cas".$p."\" size=\"3\" value=\"0\" maxlength=\"2\" onChange=\"UpdateSPP(".$p.")\"></td>\n");
 			print("   <td><input type=\"text\" name=\"bblm_int".$p."\" id=\"bblm_int".$p."\" size=\"3\" value=\"0\" maxlength=\"2\" onChange=\"UpdateSPP(".$p.")\"></td>\n");
 			print("   <td><input type=\"text\" name=\"bblm_mvp".$p."\" id=\"bblm_mvp".$p."\" size=\"3\" value=\"0\" maxlength=\"1\" onChange=\"UpdateSPP(".$p.")\"></td>\n");
-			print("   <td style=\"background-color:#ddd;\"><input type=\"text\" name=\"bblm_spp".$p."\" id=\"bblm_spp".$p."\" size=\"3\" value=\"0\" maxlength=\"2\"></td>\n");
+			if ( $ccounts ) {
+				print("   <td style=\"background-color:#ddd;\"><input type=\"text\" name=\"bblm_spp".$p."\" id=\"bblm_spp".$p."\" size=\"3\" value=\"0\" maxlength=\"2\"></td>\n");
+			}
+			else {
+				print("   <td style=\"background-color:#ddd;\"><input type=\"hidden\" name=\"bblm_spp".$p."\" id=\"bblm_spp".$p."\" size=\"3\" value=\"0\" maxlength=\"2\">N/A</td>\n");
+			}
 			print("   <td><input type=\"hidden\" name=\"bblm_oldspp".$p."\" id=\"bblm_oldspp".$p."\" size=\"3\" value=\"".$pl->p_spp."\">".$pl->p_spp."</td>\n");
 			print("   <td><input type=\"checkbox\" name=\"bblm_plyd".$p."\" checked=\"checked\"></td>\n   <td><input type=\"checkbox\" name=\"mng".$p."\"></td>\n ");
-			print("   <td><input type=\"text\" name=\"bblm_increase".$p."\" id=\"bblm_increase".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n   <td><input type=\"text\" name=\"bblm_injury".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n");
+			if ( $ccounts ) {
+				print("   <td><input type=\"text\" name=\"bblm_increase".$p."\" id=\"bblm_increase".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n");
+			}
+			else {
+				print("   <td><input type=\"hidden\" name=\"bblm_increase".$p."\" id=\"bblm_increase".$p."\" size=\"10\" value=\"\" maxlength=\"30\">Exhibition game</td>\n");
+			}
+
+			print("   <td><input type=\"text\" name=\"bblm_injury".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n");
 			print("  </tr>\n");
 
 			$p++;
@@ -517,17 +540,17 @@ $playersql = 'SELECT P.p_id, P.t_id, P.p_spp, X.post_title AS p_name, P.p_num, T
 		//Determine the Star Player Team (to get the star players)
 		$bblm_team_star = bblm_get_star_player_team();
 
-		$starssql = 'SELECT P.post_name, X.p_id FROM '.$wpdb->prefix.'player X, '.$wpdb->prefix.'posts P, '.$wpdb->prefix.'bb2wp J WHERE J.prefix = \'p_\' AND J.pid = P.ID AND J.tid = X.p_id AND X.t_id = '.$bblm_team_star.' order by P.post_name ASC';
+		$starssql = 'SELECT P.post_title, X.p_id FROM '.$wpdb->prefix.'player X, '.$wpdb->prefix.'posts P, '.$wpdb->prefix.'bb2wp J WHERE J.prefix = \'p_\' AND J.pid = P.ID AND J.tid = X.p_id AND X.t_id = '.$bblm_team_star.' order by P.post_name ASC';
 		if ($stars = $wpdb->get_results($starssql)) {
 			$starlist = "";
 			foreach ($stars as $star) {
-				$starlist .= "<option value=\"".$star->p_id."\">".$star->post_name."</option>\n";
+				$starlist .= "<option value=\"".$star->p_id."\">" . esc_html( $star->post_title ) . "</option>\n";
 			}
 			//create a drop down containing the two teams.
 			$teamlist = "	<option value=\"".$tAid."\">".$teamA."</option>\n	<option value=\"".$tBid."\">".$teamB."</option>\n";
 
 			$pmax = $p+4;
-			print("	<h3>Star Players</h3>\n	<p>If Any Star Players took part in the match enter their details below and tick the played box.</p>\n	<table cellspacing=\"0\" class=\"widefat\">\n		<tr>\n			<th>Team</th>\n			<th>Star</th>\n			<th>TD</th>\n			<th>COMP</th>\n			<th>CAS</th>\n			<th>INT</th>\n			<th>MVP</th>\n			<th>SPP</th>\n			<th>Played?</th>\n			<th>Increase? (keep blank)</th>\n</tr>\n");
+			print("	<h3>Star Players</h3>\n	<p>If Any Star Players took part in the match enter their details below and tick the played box.</p>\n	<table cellspacing=\"0\" class=\"widefat\">\n		<tr>\n			<th>Team</th>\n			<th>Star</th>\n			<th>TD</th>\n			<th>COMP</th>\n			<th>CAS</th>\n			<th>INT</th>\n			<th>MVP</th>\n			<th>SPP</th>\n			<th>Played?</th>\n			</tr>\n");
 			while ($p < $pmax){
 
 				if ($p % 2) {
@@ -544,7 +567,10 @@ $playersql = 'SELECT P.p_id, P.t_id, P.p_spp, X.post_title AS p_name, P.p_num, T
 				print("			<td><input type=\"text\" name=\"bblm_int".$p."\" id=\"bblm_int".$p."\" size=\"3\" value=\"0\" maxlength=\"2\" onChange=\"UpdateSPP(".$p.")\"></td>\n");
 				print("			<td><input type=\"text\" name=\"bblm_mvp".$p."\" id=\"bblm_mvp".$p."\" size=\"3\" value=\"0\" maxlength=\"1\" onChange=\"UpdateSPP(".$p.")\"></td>\n");
 				print("			<td style=\"background-color:#ddd;\"><input type=\"text\" name=\"bblm_spp".$p."\" id=\"bblm_spp".$p."\" size=\"3\" value=\"0\" maxlength=\"2\"></td>\n");
-				print("			<td><input type=\"checkbox\" name=\"bblm_plyd".$p."\"></td>\n    <input type=\"hidden\" name=\"bblm_oldspp".$p."\" id=\"bblm_oldspp".$p."\" size=\"3\" value=\"0\">   <input type=\"hidden\" name=\"mng".$p."\" id=\"mng".$p."\" size=\"3\" value=\"0\">   <td>   <input type=\"text\" name=\"bblm_increase".$p."\" id=\"bblm_increase".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n<input type=\"hidden\" name=\"bblm_injury".$p."\" size=\"10\" value=\"\" maxlength=\"30\">\n");
+				print("			<td><input type=\"checkbox\" name=\"bblm_plyd".$p."\"></td>\n");
+				print("			<input type=\"hidden\" name=\"bblm_oldspp".$p."\" id=\"bblm_oldspp".$p."\" size=\"3\" value=\"0\">");
+				print("			<input type=\"hidden\" name=\"mng".$p."\" id=\"mng".$p."\" size=\"3\" value=\"0\">");
+				print("			<input type=\"hidden\" name=\"bblm_increase".$p."\" id=\"bblm_increase".$p."\" size=\"10\" value=\"\" maxlength=\"30\"></td>\n<input type=\"hidden\" name=\"bblm_injury".$p."\" size=\"10\" value=\"\" maxlength=\"30\">\n");
 				print("		</tr>\n");
 
 				$p++;
