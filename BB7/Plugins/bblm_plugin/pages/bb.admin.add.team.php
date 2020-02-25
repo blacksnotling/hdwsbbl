158e3c2920be03c4c712dbfb173294d54c274302
blacksnotling <blacksnotling@users.noreply.github.com>
2020-02-25 23:24:34 +0000

    realinged the BB7 site with the core plugin
    Long overdue update bringing it from version 1.7 to 1.10

---
+++ BB7/Plugins/bblm_plugin/pages/bb.admin.add.team.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * BBowlLeagueMan Add Team
+ *
+ * Page used to add a new team to the league
+ *
+ * @author 		Blacksnotling
+ * @category 	Core
+ * @package 	BBowlLeagueMan/Pages
+ */
+
+//Check the file is not being accessed directly
+if (!function_exists('add_action')) die('You cannot run this file directly. Naughty Person');
+?>
+<div class="wrap">
+	<h2>Create a new Team</h2>
+
+<?php
+if(isset($_POST['bblm_team_submit'])) {
+
+	//Determine the parent page
+	$options = get_option('bblm_config');
+	$bblm_page_parent = htmlspecialchars($options['page_team'], ENT_QUOTES);
+
+	//Determine if a new Owner was created or an existing one used
+	$bblm_tuser = "";
+	if ( empty( $_POST['bblm_tusernew'] ) ) {
+
+		//If a new coach has not been provided then use the value of the drop down
+		$bblm_tuser = $_POST['bblm_tuser'];
+	}
+	else {
+		//If the NEW field is not blank then add a new Owner post (should be moved into the Owner CPT class at some point)
+		$post_id = wp_insert_post( array( 'post_title'=>sanitize_text_field( $_POST[ 'bblm_tusernew' ] ), 'post_type'=>'bblm_owner', 'post_content'=>'', 'post_status'=>'publish' ) );
+		$bblm_tuser = $post_id;
+	}
+
+	//Determine if a new Stadium was created or an existing one used
+	$bblm_tstad = "";
+	if ( empty( $_POST['bblm_tstadnew'] ) ) {
+
+		//If a new coach has not been provided then use the value of the drop down
+		$bblm_tstad = $_POST[ 'bblm_tstad' ];
+	}
+	else {
+		//If the NEW field is not blank then add a new Owner post (should be moved into the Owner CPT class at some point)
+		$post_id = wp_insert_post( array( 'post_title'=>sanitize_text_field( $_POST[ 'bblm_tstadnew' ] ), 'post_type'=>'bblm_stadium', 'post_content'=>'', 'post_status'=>'publish' ) );
+		$bblm_tstad = $post_id;
+		add_post_meta( $bblm_tstad, 'stadium_featured', 'No' );
+	}
+
+
+	$my_post = array(
+		'post_title' => wp_filter_nohtml_kses($_POST['bblm_tname']),
+		'post_content' => wp_filter_kses($_POST['bblm_tdesc']),
+		'post_type' => 'page',
+		'post_status' => 'publish',
+		'comment_status' => 'closed',
+		'ping_status' => 'closed',
+		'post_parent' => $bblm_page_parent
+	);
+	if ($bblm_submission = wp_insert_post( $my_post )) {
+		add_post_meta($bblm_submission, '_wp_page_template', 'bb.view.team.php');
+
+		//Determine permlink for this page
+		$bblmpageguid = get_permalink($bblm_submission);
+
+		$bblmdatasql = 'INSERT INTO `'.$wpdb->prefix.'team` (`t_id`, `t_name`, `r_id`, `ID`, `t_hcoach`, `t_ff`, `t_rr`, `t_apoc`, `t_cl`, `t_ac`, `t_bank`, `t_tv`, `t_active`, `t_show`, `type_id`, `t_sname`, `stad_id`, `t_img`, `t_roster`, `t_guid`, `WPID`) VALUES (\'\', \''.wp_filter_nohtml_kses($_POST['bblm_tname']).'\', \''.$_POST['bblm_trace'].'\', \''.$bblm_tuser.'\', \''.$_POST['bblm_thcoach'].'\', \''.$_POST['bblm_tff'].'\', \''.$_POST['bblm_trr'].'\', \''.$_POST['bblm_tapoc'].'\', \''.$_POST['bblm_tcl'].'\', \''.$_POST['bblm_tac'].'\', \''.$_POST['bblm_tbank'].'\', \''.$_POST['bblm_ttv'].'\', \'1\', \'1\', \''.$_POST['bblm_ttype'].'\', \''.wp_filter_nohtml_kses($_POST['bblm_sname']).'\', \''.$bblm_tstad.'\', \'\', \''.$_POST['bblm_roster'].'\', \''.$bblmpageguid.'\',  \''.$bblm_submission.'\')';
+		$wpdb->query($bblmdatasql);
+
+		$team_id = $wpdb->insert_id;
+
+		$bblmmappingsql = 'INSERT INTO `'.$wpdb->prefix.'bb2wp` (`bb2wp_id`, `tid`, `pid`, `prefix`) VALUES (\'\',\''.$team_id.'\', \''.$bblm_submission.'\', \'t_\')';
+		$wpdb->query($bblmmappingsql);
+
+
+		//Add a new Term to the database
+		wp_insert_term(
+			wp_filter_nohtml_kses($_POST['bblm_tname']), // the term
+			'post_teams' // the taxonomy
+		);
+
+		//Check to see if  roster needs generating, if it does then insert an additionl page into the database
+		$roster_added = 0;
+		if ($_POST['bblm_roster']) {
+			$my_post = array(
+				'post_title' => 'Roster',
+				'post_content' => '',
+				'post_type' => 'page',
+				'post_status' => 'publish',
+				'comment_status' => 'closed',
+				'ping_status' => 'closed',
+				'post_parent' => $bblm_submission
+			);
+			if ($bblm_submission = wp_insert_post( $my_post )) {
+				add_post_meta($bblm_submission, '_wp_page_template', 'bb.view.roster.php');
+
+				$bblmmappingsql = 'INSERT INTO `'.$wpdb->prefix.'bb2wp` (`bb2wp_id`, `tid`, `pid`, `prefix`) VALUES (\'\',\''.$team_id.'\', \''.$bblm_submission.'\', \'roster\')';
+				$wpdb->query($bblmmappingsql);
+
+				//correct the roster post title
+				$my_post = array();
+				$my_post['ID'] = $bblm_submission;
+				$my_post['post_title'] = 'Roster - '.wp_filter_nohtml_kses($_POST['bblm_tname']);
+
+				// Update the post into the database
+				wp_update_post( $my_post );
+
+				$roster_added = 1;
+			}
+		}
+
+		$success = 1;
+		$addattempt = 1;
+		do_action( 'bblm_post_submission' );
+
+	} //end of if post insertion was successful
+
+
+?>
+	<div id="updated" class="updated fade">
+		<p>
+	<?php
+	if ($success) {
+		print("Team has been created. <a href=\"".$bblmpageguid."\" title=\"View the new Team\">View page</a>.");
+		if ($roster_added) {
+			print(" A roster has also been added. <a href=\"".get_permalink($bblm_submission)."\" title=\"View the new Teams roster\">View Roster</a>");
+		}
+		print("</p>\n<p>You can now <a href=\"".site_url()."/wp-admin/admin.php?page=bblm_plugin/pages/bb.admin.add.player.php&action=add&item=none&id=".$team_id."\" title=\"Add some players to this team\">add players to this team</a> or <a href=\"".site_url()."/wp-admin/admin.php?page=bblm_plugin/pages/bb.admin.add.team.php\" title=\"Add another new team\">add another new team</a>.</p>");
+		print("<p>You can also <a href=\"");
+		echo site_url();
+		print("/wp-admin/admin.php?page=bblm_player_addbulk\" title=\"Add a new payer to the team\">Add Players in Bulk to this team</a></p>\n");
+		echo '<p>If you added a new Stadium, <a href="' . site_url() . '/wp-admin/post.php?post=' . $bblm_tstad . '&action=edit">then add a description to the stadium.</a>';
+	}
+	else {
+		print("Something went wrong! Please try again.");
+	}
+
+
+?>
+		</p>
+	</div>
+<?php
+//end of submit if
+}
+else if(isset($_POST['bblm_race_select'])) {
+	  ///////////////////////////////
+	 // Begin Output of main form //
+	///////////////////////////////
+?>
+
+	<form name="bblm_addteam" method="post" id="post">
+
+	<table class="form-table">
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tname">Team Name</label></th>
+		<td><input type="text" name="bblm_tname" size="50" value="" id="bblm_tname" maxlength="50" class="large-text"/></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row"><label for="bblm_tdesc">Bio</label></th>
+		<td><p><textarea rows="10" cols="50" name="bblm_tdesc" id="bblm_tdesc" class="large-text"></textarea></p></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_sname">Short Name</label></th>
+		<td><input type="text" name="bblm_sname" size="3" value="" id="bblm_sname" maxlength="5" class="small-text"/><br />
+		This will be displayed on various reports and pages.</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tuser"><?php echo __( 'Owner', 'bblm' ); ?></label></th>
+		<td><select name="bblm_tuser" id="bblm_tuser">
+<?php
+		//Grabs a list of 'posts' from the Owners CPT
+		$oposts = get_posts(
+			array(
+				'post_type' => 'bblm_owner',
+				'numberposts' => -1,
+				'orderby' => 'post_title',
+				'order' => 'ASC'
+			)
+		);
+		if( ! $oposts ) return;
+		foreach( $oposts as $o ) {
+			echo '<option value="' . $o->ID . '">' . bblm_get_owner_name( $o->ID ) . '</option>';
+		}
+
+?>
+		</select>
+		<label for="bblm_tusernew"><?php echo __( ' OR Add a new one:', 'bblm' ); ?></label>
+		<input type="text" name="bblm_tusernew" id="bblm_tusernew" size="25" value="" maxlength="25" class="large-text" placeholder="New Owner - leave this message (I.E blank) to ignore"/></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_thcoach">Head Coach</label></th>
+		<td><input type="text" name="bblm_thcoach" size="25" value="Unkown" maxlength="25" class="large-text"/></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tstad"><?php echo __( 'Home Stadium', 'bblm' ); ?></label></th>
+		<td><select name="bblm_tstad" id="bblm_tstad">
+			<?php
+					//Grabs a list of 'posts' from the Stadiums CPT
+					$oposts = get_posts(
+						array(
+							'post_type' => 'bblm_stadium',
+							'numberposts' => -1,
+							'orderby' => 'post_title',
+							'order' => 'ASC'
+						)
+					);
+					if( ! $oposts ) return;
+					foreach( $oposts as $o ) {
+						echo '<option value="' . $o->ID . '">' . bblm_get_stadium_name( $o->ID ) . '</option>';
+					}
+
+			?></select>
+			<label for="bblm_tstadnew"><?php echo __( ' OR Add a new one:', 'bblm' ); ?></label>
+			<input type="text" name="bblm_tstadnew" id="bblm_tstadnew" size="25" value="" maxlength="25" class="large-text" placeholder="New Stadium - leave this message (I.E blank) to ignore"/></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_roster">Generate Roster?</label></th>
+		<td><select name="bblm_roster" id="bblm_roster">
+			<option value="1">Yes</option>
+			<option value="0">No</option>
+		</select></td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_ttype">Team Type</label></th>
+		  <td><select name="bblm_ttype" id="bblm_tuser">
+<?php
+		$typesql = 'SELECT type_id, type_name FROM '.$wpdb->prefix.'team_type ORDER BY type_id';
+		if ($types = $wpdb->get_results($typesql)) {
+			foreach ($types as $type) {
+				print("<option value=\"$type->type_id\">".$type->type_name."</option>\n");
+			}
+		}
+?>
+		</select></td>
+	</tr>
+
+	</table
+
+
+	<h3>Initial Purchases</h3>
+
+<script type="text/javascript">
+function UpdateBankTv() {
+	var tot_rr = document.getElementById('bblm_trr').value * document.getElementById('bblm_trrcost').value;
+
+	var tot_ff = document.getElementById('bblm_tff').value * 10000;
+
+	var tot_cl = document.getElementById('bblm_tcl').value * 10000;
+
+	var tot_ac = document.getElementById('bblm_tac').value * 10000;
+
+	var tot_apoc = document.getElementById('bblm_tapoc').value * 50000;
+
+	var tot_tv = tot_rr + tot_ff + tot_cl + tot_ac + tot_apoc;
+	document.getElementById('bblm_ttv').value = tot_tv;
+
+	var tot_bank = 1000000 - tot_tv;
+	document.getElementById('bblm_tbank').value = tot_bank;
+}
+</script>
+
+<?php
+		$racesql = "SELECT r_id, r_name, r_rrcost FROM ".$wpdb->prefix."race WHERE r_id = ".$_POST['bblm_rid'];
+		if ($races = $wpdb->get_results($racesql)) {
+			foreach ($races as $race) {
+				$rid = $race->r_id;
+				$rrcost = $race->r_rrcost;
+			}
+		}
+?>
+
+	<table class="form-table">
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_trr">Re-Rolls</label></th>
+		<td><input type="text" name="bblm_trr" size="2" value="0" maxlength="1" id="bblm_trr" class="small-text"/><br />
+		@ <?php print($rrcost); ?> each</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tff">Fan Factor</label></th>
+		  <td><input type="text" name="bblm_tff" size="2" value="0" maxlength="2" id="bblm_tff" class="small-text"/><br />
+		  @ 10,000 each</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tcl">Cheerleaders</label></th>
+		  <td><input type="text" name="bblm_tcl" size="2" value="0" maxlength="2" id="bblm_tcl" class="small-text"/><br />
+		  @ 10,000 each</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tac">Assistant Coaches</label></th>
+		  <td><input type="text" name="bblm_tac" size="2" value="0" maxlength="2" id="bblm_tac" class="small-text"/><br />
+		  @ 10,000 each</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tapoc">Apothecary</label></th>
+		  <td><input type="text" name="bblm_tapoc" size="1" value="0" maxlength="1" id="bblm_tapoc" class="small-text"/><br />
+		  @ 10,000 each</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top">&nbsp;</th>
+		  <td><input type="button" value="Update Bank + TV" onClick="UpdateBankTv();"/></td>
+	</tr>
+
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_tbank">Remaining Bank</label></th>
+		  <td><input type="text" name="bblm_tbank" size="7" value="600000" maxlength="7" id="bblm_tbank"/>gp</td>
+	</tr>
+	<tr valign="top">
+		<th scope="row" valign="top"><label for="bblm_ttv">Team Value (initial)</label></th>
+		  <td><input type="text" name="bblm_ttv" size="7" value="0" maxlength="7" id="bblm_ttv"/>gp</td>
+	</tr>
+	</table>
+
+	<input type="hidden" name="bblm_trace" size="3" value="<?php print($rid); ?>"/>
+	<input type="hidden" name="bblm_trrcost" id="bblm_trrcost" maxlength="6" value="<?php print($rrcost); ?>"/>
+
+	<p class="submit"><input type="submit" name="bblm_team_submit" value="Create Team" title="Add the Team" class="button-primary"/></p>
+</form>
+
+<?php
+}//end of elseIF
+else {
+?>
+	<form name="bblm_addposition" method="post" id="post">
+
+	<p>The following page is used to add a new team to the league.</p>
+	<p>Before you continue, please ensure that you have created a user ID for the coach and set up a stadium.</p>
+	<p>Before you can begin creating the new team, you must first select the Race of the new team:</p>
+
+
+	<table class="form-table">
+	<tr valign="top">
+		<th scope="row"><label for="bblm_rid">Race</label></th>
+		<td><select name="bblm_rid" id="bblm_rid">
+<?php
+		$racesql = "SELECT R.r_id, R.r_name FROM ".$wpdb->prefix."race R, ".$wpdb->prefix."bb2wp J, ".$wpdb->posts." P WHERE R.r_id = J.tid AND J.pid = P.ID AND J.prefix = 'r_' ORDER BY R.r_name ASC";
+			if ($races = $wpdb->get_results($racesql)) {
+				foreach ($races as $race) {
+					print("			<option value=\"$race->r_id\">".$race->r_name."</option>\n");
+				}
+			}
+?>
+		</select></td>
+	</tr>
+	</table>
+
+	<p class="submit"><input type="submit" name="bblm_race_select" value="Select above Race" title="Select the above Race" class="button-primary"/></p>
+	</form>
+<?php
+} //end of else section
+?>
+</div>
--- BB7/Plugins/bblm_plugin/templates/single-bblm_roster.php
+++ BB7/Plugins/bblm_plugin/templates/single-bblm_roster.php
@@ -1,30 +1,22 @@
 <?php
+/**
+ * BBowlLeagueMan Teamplate View Roster
+ *
+ * Page Template to view a Roster
+ *
+ * @author 		Blacksnotling
+ * @category 	Template
+ * @package 	BBowlLeagueMan/Templates
+ */
 /*
-Template Name: Team Roster
-*/
-/*
-*	Filename: bb.view.roster.php
-*	Version: 1.3
-*	Description: .Page template to display
-*/
-/* -- Change History --
-20080622 - 1.0b - Initial creation of file.
-20080623 - 1.0.1b - Fixed some validation errors
-20080722 - 1.1b - changed MNG from 1 to Y, added some more number formattion
-20080730 - 1.0 - bump to Version 1 for public release.
-20090117 - 1.1 - Changed title tag to have roster name then BBBBL
-			   - Added Injuries to the roster
-			   - The Team Captain (if assigned) is highlighted.
-20090331 - 1.2 - The Roster will display the team logo if present and the race logo if not
-20090824 - 1.2.1 - re-arranged the HTML title tag as it began with arrows and was annoying me!
-20100123 - 1.3 - Updated the prefix for the custom bb tables in the Database (tracker [225])
-*/
+ * Template Name: View Roster
+ */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+<html <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title><?php wp_title(); ?> - BBBBL </title>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
 <?php wp_head(); ?>
 <style type="text/css">
 html * {
@@ -107,20 +99,23 @@ a:hover, a:active {
 	<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
 
-<?
-		$teaminfosql = 'SELECT T.*, J.tid AS teamid, R.r_name, R.r_rrcost, L.guid AS racelink, U.display_name, W.post_title AS stad, W.guid AS stadlink, H.guid AS TeamLink, H.post_title AS TeamName FROM '.$wpdb->prefix.'team T, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' P, '.$wpdb->users.' U, '.$wpdb->prefix.'race R, '.$wpdb->prefix.'bb2wp K, '.$wpdb->posts.' L, '.$wpdb->prefix.'bb2wp Q, '.$wpdb->posts.' W, '.$wpdb->prefix.'bb2wp Y, '.$wpdb->posts.' H WHERE T.t_id = Y.tid AND Y.prefix = \'t_\' AND Y.pid = H.ID AND Y.tid = J.tid AND T.stad_id = Q.tid AND Q.prefix = \'stad_\' AND Q.pid = W.ID AND T.r_id = K.tid AND K.prefix = \'r_\' AND K.pid = L.ID AND T.ID = U.ID AND R.r_id = T.r_id AND T.t_id = J.tid AND J.prefix = \'roster\' AND J.pid = P.ID AND P.ID = '.$post->ID;
+<?php
+		$teaminfosql = 'SELECT T.*, J.tid AS teamid, R.r_name, R.r_rrcost, L.guid AS racelink, T.stad_id, T.WPID FROM '.$wpdb->prefix.'team T, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' P, '.$wpdb->prefix.'race R, '.$wpdb->prefix.'bb2wp K, '.$wpdb->posts.' L WHERE T.t_id = J.tid AND T.r_id = K.tid AND K.prefix = \'r_\' AND K.pid = L.ID AND R.r_id = T.r_id AND T.t_id = J.tid AND J.prefix = \'roster\' AND J.pid = P.ID AND P.ID = '.$post->ID;
 		if ($ti = $wpdb->get_row($teaminfosql)) {
 				$tid = $ti->teamid;
+				$team_name = esc_html( get_the_title( $ti->WPID ) );
+				$team_link = get_post_permalink( $ti->WPID );
 
 			//determine Team Captain
+			$teamcap = 0;
 			$teamcaptainsql = 'SELECT * FROM '.$wpdb->prefix.'team_captain WHERE tcap_status = 1 and t_id = '.$tid;
 			if ($tcap = $wpdb->get_row($teamcaptainsql)) {
 				$teamcap = $tcap->p_id;
 			}
 ?>
-		<h1>Roster for <a href="<?php print($ti->TeamLink); ?>" title="Read more about <?php print($ti->TeamName); ?>"><?php print($ti->TeamName); ?></a></h1>
+		<h1>Roster for <a href="<?php print( $team_link ); ?>" title="Read more about <?php print( $team_name ); ?>"><?php print( $team_name ); ?></a></h1>
 
-<?
+<?php
 		}
 ?>
 <table border="0">
@@ -175,13 +170,13 @@ a:hover, a:active {
 				//checks to see the player belongs in this position
 				if ($pcount == $pl->p_num) {
 
-				$playerdetailssql = 'SELECT SUM(M.mp_td) AS PTD, SUM(M.mp_cas) AS PCAS, SUM(M.mp_comp) AS PCOMP, SUM(M.mp_int) AS PINT, SUM(M.mp_MVP) AS PMVP FROM '.$wpdb->prefix.'player P, '.$wpdb->prefix.'match_player M, '.$wpdb->prefix.'match N, '.$wpdb->prefix.'comp C, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' O WHERE M.m_id = N.m_id AND N.c_id = C.c_id AND C.c_counts = 1 AND C.c_show = 1 AND P.p_id = J.tid AND J.prefix = \'p_\' AND J.pid = O.ID AND M.p_id = P.p_id AND M.mp_spp > 0 AND P.p_id = '.$pl->p_id;
+				$playerdetailssql = 'SELECT SUM(M.mp_td) AS PTD, SUM(M.mp_cas) AS PCAS, SUM(M.mp_comp) AS PCOMP, SUM(M.mp_int) AS PINT, SUM(M.mp_MVP) AS PMVP, SUM(M.mp_spp) AS PSPP FROM '.$wpdb->prefix.'player P, '.$wpdb->prefix.'match_player M, '.$wpdb->prefix.'match N, '.$wpdb->prefix.'comp C, '.$wpdb->prefix.'bb2wp J, '.$wpdb->posts.' O WHERE M.m_id = N.m_id AND N.c_id = C.c_id AND C.c_counts = 1 AND C.c_show = 1 AND P.p_id = J.tid AND J.prefix = \'p_\' AND J.pid = O.ID AND M.p_id = P.p_id AND M.mp_spp > 0 AND P.p_id = '.$pl->p_id;
 				$pd = $wpdb->get_row($playerdetailssql)
 ?>
 <tr>
   <td><?php print($pcount); ?></td>
   <td><?php print("<a href=\"".$pl->guid."\" title=\"View more information about ".$pl->post_title."\">".$pl->post_title."</a>"); if ($teamcap == $pl->p_id) { print(" (C)");} ?></td>
-  <td><?php print($pl->pos_name); ?></td>
+  <td><?php print( esc_html( $pl->pos_name ) ); ?></td>
   <td><?php print($pl->p_ma); ?></td>
   <td><?php print($pl->p_st); ?></td>
   <td><?php print($pl->p_ag); ?></td>
@@ -231,15 +226,15 @@ a:hover, a:active {
 		print("  <td>".$pd->PMVP."</td>\n");
 	}
 ?>
-  <td><?php print($pl->p_spp); ?></td>
+  <td><?php echo $pd->PSPP; ?></td>
   <td class="tbl_value"><?php print(number_format($pl->p_cost_ng)); ?>gp</td>
  </tr>
 <?php
 				}
 				$pcount++;
 			}
-		//now the sql has been completed, we have to print emtpy lines to ensure that 16 places are displayed!
-			while (17 > $pcount) {
+		//now the sql has been completed, we have to print emtpy lines to ensure that 11 places are displayed!
+			while (12 > $pcount) {
 				//print a generic row
 ?>
  <tr>
@@ -272,17 +267,17 @@ a:hover, a:active {
 	$filename = $_SERVER['DOCUMENT_ROOT']."/images/teams/".$ti->t_sname."_big.gif";
 	if (file_exists($filename)) {
 ?>
-  <td colspan="3" rowspan="7" class="tbl_logo"><img src="<?php print(get_option('home')); ?>/images/teams/<?php print($ti->t_sname); ?>_big.gif" alt="Team Logo" /></td>
+  <td colspan="3" rowspan="7" class="tbl_logo"><img src="<?php print(home_url()); ?>/images/teams/<?php print($ti->t_sname); ?>_big.gif" alt="Team Logo" /></td>
 <?php
 	}
 	else {
 ?>
-  <td colspan="3" rowspan="7" class="tbl_logo"><img src="<?php print(get_option('home')); ?>/images/races/race<?php print($ti->r_id); ?>.gif" alt="<?php print($ti->r_name); ?> Logo" /></td>
+  <td colspan="3" rowspan="7" class="tbl_logo"><img src="<?php print(home_url()); ?>/images/races/race<?php print($ti->r_id); ?>.gif" alt="<?php print($ti->r_name); ?> Logo" /></td>
 <?php
 	}
 ?>
   <th colspan="4" rowspan="2" class="tbl_title">Team Name:</th>
-  <td rowspan="2"><a href="<?php print($ti->TeamLink); ?>" title="Read more about <?php print($ti->TeamName); ?>"><?php print($ti->TeamName); ?></a></td>
+  <td rowspan="2"><a href="<?php print( $team_link ); ?>" title="Read more about <?php print( $team_name ); ?>"><?php print( $team_name ); ?></a></td>
   <th colspan="3" class="tbl_title">Re-Rolls:</th>
   <td><?php print($ti->t_rr); ?></td>
   <th class="tbl_enchance">X</th>
@@ -322,11 +317,12 @@ a:hover, a:active {
   <td class="tbl_value"><?php print(number_format($ti->t_apoc*50000)); ?>gp</td>
  </tr>
  <tr>
-  <td height="24" colspan="8">&nbsp;</td>
+	<th colspan="3" class="tbl_title">Stadium:</th>
+	<td height="24" colspan="5"><?php echo bblm_get_stadium_link( $ti->stad_id ); ?></td>
  </tr>
  <tr>
   <th colspan="4" class="tbl_title">Head Coach:</th>
-  <td><?php print($ti->t_hcoach); ?> (<?php print($ti->display_name); ?>)</td>
+  <td><?php print($ti->t_hcoach); ?> (<?php echo esc_html( get_the_title( $tid = $ti->ID ) ); ?>)</td>
   <th colspan="7" class="tbl_title">Total Value of Team (TV):</th>
   <td class="tbl_value"><?php print(number_format($ti->t_tv)); ?>gp</td>
  </tr>
@@ -338,7 +334,7 @@ a:hover, a:active {
 		</div> <!-- End of #maincontent -->
 	</div> <!-- End of #pagecontent -->
 	<div id="footer">
-				<p>Unique content is &copy; <a href="<?php echo get_option('home'); ?>" title="Visit the homepage of the BBBBL">BBBBL</a> 2006 - present.</p>
+				<p>Unique content is &copy; <a href="<?php echo home_url(); ?>" title="Visit the homepage of the <?php echo bblm_get_league_name(); ?>"><?php echo bblm_get_league_name(); ?></a> 2006 - present.</p>
 				<p>Blood Bowl concept and miniatures are &copy; Games Workshop LTD used without permission.</p>
 				<?php wp_footer(); ?>
 	</div> <!-- End of #footer -->
