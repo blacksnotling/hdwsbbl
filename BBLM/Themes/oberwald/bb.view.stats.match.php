c4a4749f6f5a55e4f386f2fe2d41ac778f4c9896
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2020-03-28 23:36:27 +0000

    Deleting files made obsolete in the last version

--- BBLM/Themes/oberwald/bb.view.stats.match.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="wrap">
-	<h2>Obsolete!</h2>
-		<p>You have come here in error. This file is not in use and will be deleted in a future version.</p>
-</div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
