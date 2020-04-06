7ce57b4260638b300dfea5e1f49352ec94f64613
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2020-04-06 23:11:21 +0100

    finished off single view and associated functions

--- BBLM/Plugins/bblm_plugin/includes/post-types/class-bblm-cpt-race.php
+++ BBLM/Plugins/bblm_plugin/includes/post-types/class-bblm-cpt-race.php
@@ -118,7 +118,7 @@ class BBLM_CPT_Race {
   * Returns the number of teams who represented this race
   *
   * @param wordpress $query
-  * @return int $count Number of games played
+  * @return int $teamnum Numnber of teams
   */
   public function get_number_teams( $ID ) {
     global $post;
@@ -132,10 +132,24 @@ class BBLM_CPT_Race {
   }// end of get_number_teams()
 
 	/**
+   * Returns the cost of Rerolls for a race
+   *
+   * @param wordpress $query
+   * @return int $rr_cost cost of reroll
+   */
+   public static function get_reroll_cost( $ID ) {
+
+		 $rr_cost = get_post_meta( $ID, 'race_rrcost', true );
+
+     return $rr_cost;
+
+   }// end of get_reroll_cost()
+
+	/**
    * OUTPUTS the race icon to a set size
    *
    * @param wordpress $query
-   * @return int $count Number of games played
+   * @return none
    */
    public function display_race_icon( $ID, $size ) {
 
@@ -153,7 +167,165 @@ class BBLM_CPT_Race {
 
      echo get_the_post_thumbnail( $ID, 'bblm-fit-' . $size );
 
-   }// end of get_number_teams()
+   }// end of display_race_icon()
+
+	 /**
+	  * Outputs the positions that can play for a race
+	  *
+	  * @param int $id the race in question
+	  * @return none
+	  */
+	  public function display_race_positions( $ID ) {
+			global $wpdb;
+
+			$positionsql = 'SELECT * FROM '.$wpdb->prefix.'position WHERE pos_status = 1 AND r_id = ' . $ID . ' ORDER by pos_cost ASC';
+			if ( $positions = $wpdb->get_results( $positionsql ) ) {
+				$zebracount = 1;
+?>
+			<table class="bblm_table bblm-tbl-scrollable">
+				<thead>
+					<tr>
+						<th><?php echo __( 'Position', 'bblm' ); ?></th>
+						<th><?php echo __( 'Limit', 'bblm' ); ?></th>
+						<th class="bblm_tbl_stat"><?php echo __( 'MA', 'bblm' ); ?></th>
+						<th class="bblm_tbl_stat"><?php echo __( 'ST', 'bblm' ); ?></th>
+						<th class="bblm_tbl_stat"><?php echo __( 'AG', 'bblm' ); ?></th>
+						<th class="bblm_tbl_stat"><?php echo __( 'AV', 'bblm' ); ?></th>
+						<th><?php echo __( 'Skills', 'bblm' ); ?></th>
+						<th><?php echo __( 'Cost', 'bblm' ); ?></th>
+					</tr>
+				</thead>
+				<tbody>
+<?php
+				foreach ( $positions as $pos ) {
+					if ( $zebracount % 2 ) {
+						echo '<tr id="pos-' . $pos->pos_id . '">';
+					}
+					else {
+						echo '<tr class="bblm_tbl_alt" id="pos-' . $pos->pos_id . '">';
+					}
+?>
+						<td><?php echo esc_html( $pos->pos_name ); ?></td>
+						<td>0 - <?php echo $pos->pos_limit; ?></td>
+						<td><?php echo $pos->pos_ma; ?></td>
+						<td><?php echo $pos->pos_st; ?></td>
+						<td><?php echo $pos->pos_ag; ?></td>
+						<td><?php echo $pos->pos_av; ?></td>
+						<td class="bblm_tbl_skills"><?php echo $pos->pos_skills; ?></td>
+						<td><?php echo number_format( $pos->pos_cost ); ?> GP</td>
+					</tr>
+<?php
+					$zebracount++;
+				}
+?>
+						</tbody>
+					</table>
+<?php
+			}
+			else {
+				print("	<div class=\"bblm_info\">\n		<p>Sorry, but no positions have been filled out for this race</p>\n	</div>\n");
+			}
+
+	  } //end of display_race_positions()
+
+		/**
+ 	  * Outputs the Star Players availbile to a race
+ 	  *
+ 	  * @param int $id the race in question
+ 	  * @return none
+ 	  */
+ 	  public function display_stars_available( $ID ) {
+ 			global $wpdb;
+
+			$starplayersql = 'SELECT X.WPID AS PWPID, X.p_ma, X.p_st, X.p_ag, X.p_av, X.p_skills, X.p_cost FROM '.$wpdb->prefix.'race2star S, '.$wpdb->prefix.'player X WHERE X.p_id = S.p_id AND S.r_id = ' . $ID . ' AND X.p_status = 1 ORDER BY X.p_name ASC';
+			if ( $starplayer = $wpdb->get_results( $starplayersql ) ) {
+				$zebracount = 1;
+?>
+					<table class="bblm_table bblm-tbl-scrollable">
+						<thead>
+							<tr>
+								<th><?php echo __( 'Name', 'bblm' ); ?></th>
+								<th class="bblm_tbl_stat"><?php echo __( 'MA', 'bblm' ); ?></th>
+								<th class="bblm_tbl_stat"><?php echo __( 'ST', 'bblm' ); ?></th>
+								<th class="bblm_tbl_stat"><?php echo __( 'AG', 'bblm' ); ?></th>
+								<th class="bblm_tbl_stat"><?php echo __( 'AV', 'bblm' ); ?></th>
+								<th><?php echo __( 'Skills', 'bblm' ); ?></th>
+								<th><?php echo __( 'Cost', 'bblm' ); ?></th>
+							</tr>
+						</thead>
+						<tbody>
+<?php
+				foreach ( $starplayer as $star ) {
+					if ( $zebracount % 2 ) {
+						if ( $zebracount % 2 ) {
+							echo '<tr>';
+						}
+						else {
+							echo '<tr class="bblm_tbl_alt">';
+						}
+					}
+						?>
+								<td><?php echo bblm_get_player_link( $star->PWPID ); ?></td>
+								<td><?php echo $star->p_ma; ?></td>
+								<td><?php echo $star->p_st; ?></td>
+								<td><?php echo $star->p_ag; ?></td>
+								<td><?php echo $star->p_av; ?></td>
+								<td class="bblm_tbl_skills"><?php echo $star->p_skills; ?></td>
+								<td><?php number_format( $star->p_cost ); ?> GP</td>
+							</tr>
+						<?php
+					$zebracount++;
+				 }
+?>
+						</tbody>
+					</table>
+<?php
+			}
+
+ 	  } //end of display_stars_available()
+
+		/**
+ 	  * Outputs the Teams representing a race
+ 	  *
+ 	  * @param int $id the race in question
+ 	  * @return none
+ 	  */
+ 	  public function display_teams_representing( $ID ) {
+ 			global $wpdb;
+
+			$teamsql = 'SELECT T.WPID AS TWPID FROM '.$wpdb->prefix.'team T WHERE T.t_show = 1 AND T.r_id = ' . $ID . ' ORDER by T.t_name ASC';
+			if ($teams = $wpdb->get_results( $teamsql ) ) {
+				$zebracount = 1;
+?>
+					<table class="bblm_table bblm-tbl-scrollable">
+						<thead>
+							<tr>
+								<th><?php echo __( 'Team', 'bblm' ); ?></th>
+							</tr>
+						</thead>
+						<tbody>
+<?php
+				foreach ( $teams as $td ) {
+					if ( $zebracount % 2 ) {
+						if ( $zebracount % 2 ) {
+							echo '<tr>';
+						}
+						else {
+							echo '<tr class="bblm_tbl_alt">';
+						}
+					}
+
+					echo '<td>' . bblm_get_team_link( $td->TWPID ) . '</td>';
+					echo '</tr>';
+				}
+				echo '</tbody>';
+				echo '</table>';
+			}
+			else {
+				echo '<div class="bblm_info">' . __( 'There are currently no teams representing this Race.', 'bblm' ) . '</div>';
+			}
+
+ 	  } //end of display_teams_representing()
 
 } //end of class
 
--- BBLM/Plugins/bblm_plugin/templates/single-bblm_race.php
+++ BBLM/Plugins/bblm_plugin/templates/single-bblm_race.php
@@ -33,81 +33,24 @@
 					<?php the_content(); ?>
 				</div>
 <?php
-				$racesql = "SELECT P.*, R.r_rrcost, R.r_id FROM ".$wpdb->prefix."race R, ".$wpdb->posts." P, ".$wpdb->prefix."bb2wp J WHERE R.r_id = J.tid AND P.ID = J.pid AND P.ID = ".$post->ID;
-				if ( $races = $wpdb->get_results( $racesql ) ) {
-          echo '<ul>';
-					foreach ( $races as $race ) {
-            echo '<li><strong>' . __( 'Re-Roll Cost:', 'bblm' ) . '</strong> ' . number_format( $race->r_rrcost ) . ' GP</li>';
-						$race_id = $race->r_id;
-					}
-          echo '</ul>';
-				}
-				else {
-          echo '<ul>';
-          echo '<li><strong>' . __( 'Re-Roll Cost:', 'bblm' ) . '</strong> ' . __( 'Not Availible', 'bblm' ) . '</li>';
-          echo '</ul>';
-				}
+        $race_id = get_the_ID();
+        $rr_cost = BBLM_CPT_Race::get_reroll_cost( $race_id );
 
-				if ( isset( $race_id ) ) {
-					//we only want to continue if the above selection returned something.
-          echo '<h3 class="bblm-table-caption">' . __( 'Positions available for Race', 'bblm' ) . '</h3>';
-					//Grab Positions
-					$positionsql = 'SELECT * FROM '.$wpdb->prefix.'position WHERE pos_status = 1 AND r_id = '.$race_id.' ORDER by pos_cost ASC';
-					if ($positions = $wpdb->get_results($positionsql)) {
-						$zebracount = 1;
-						print("<table class=\"bblm_table\">\n	<tr>\n		<th>Name</th>\n		<th>Limit</th>\n		<th class=\"bblm_tbl_stat\">MA</th>\n		<th class=\"bblm_tbl_stat\">ST</th>\n		<th class=\"bblm_tbl_stat\">AG</th>\n		<th class=\"bblm_tbl_stat\">AV</th>\n		<th>Skills</th>\n		<th>Cost</th>\n	</tr>\n");
-						foreach ($positions as $pos) {
-							if ($zebracount % 2) {
-								print("		<tr id=\"pos-".$pos->pos_id."\">\n");
-							}
-							else {
-								print("	<tr class=\"bblm_tbl_alt\" id=\"pos-".$pos->pos_id."\">\n");
-							}
-							print("		<td>" . esc_html( $pos->pos_name ) . "</td>\n		<td>0 - ".$pos->pos_limit."</td>\n		<td>".$pos->pos_ma."</td>\n		<td>".$pos->pos_st."</td>\n		<td>".$pos->pos_ag."</td>\n		<td>".$pos->pos_av."</td>\n		<td class=\"bblm_tbl_skills\">".$pos->pos_skills."</td>\n		<td>".number_format($pos->pos_cost)."gp</td>\n	</tr>\n");
-							$zebracount++;
-						}
-						print("</table>\n");
-					}
-					else {
-						print("	<div class=\"bblm_info\">\n		<p>Sorry, but no positions have been filled out for this race</p>\n	</div>\n");
-					}
+        echo '<ul>';
+        echo '<li><strong>' . __( 'Re-Roll Cost:', 'bblm' ) . '</strong> ' . number_format( $rr_cost ) . ' GP</li>';
+        echo '</ul>';
 
-					//Availible Star Players
-					$starplayersql = 'SELECT P.post_title, P.guid, X.p_ma, X.p_st, X.p_ag, X.p_av, X.p_skills, X.p_cost FROM '.$wpdb->prefix.'race2star S, '.$wpdb->prefix.'posts P, '.$wpdb->prefix.'bb2wp J, '.$wpdb->prefix.'player X WHERE J.pid = P.ID AND J.prefix = \'p_\' AND J.tid = S.p_id AND X.p_id = S.p_id AND S.r_id = '.$race_id.' AND X.p_status = 1 ORDER BY P.post_title ASC LIMIT 0, 30 ';
-					if ($starplayer = $wpdb->get_results($starplayersql)) {
-						$zebracount = 1;
-						print("<h3>Star Players available for Race</h3>\n");
-						print("<table class=\"bblm_table\">\n	<tr>\n		<th>Name</th>\n		<th class=\"bblm_tbl_stat\">MA</th>\n		<th class=\"bblm_tbl_stat\">ST</th>\n		<th class=\"bblm_tbl_stat\">AG</th>\n		<th class=\"bblm_tbl_stat\">AV</th>\n		<th>Skills</th>\n		<th>Cost</th>\n	</tr>\n");
-						foreach ($starplayer as $star) {
-							if ($zebracount % 2) {
-								print("		<tr>\n");
-							}
-							else {
-								print("	<tr class=\"bblm_tbl_alt\">\n");
-							}
-							print("		<td><a href=\"".$star->guid."\" title=\"See more details of this player\">".$star->post_title."</a></td>\n		<td>".$star->p_ma."</td>\n		<td>".$star->p_st."</td>\n		<td>".$star->p_ag."</td>\n		<td>".$star->p_av."</td>\n		<td class=\"bblm_tbl_skills\">".$star->p_skills."</td>\n		<td>".number_format($star->p_cost)."gp</td>\n	</tr>\n");
-							$zebracount++;
-						}
-						print("</table>\n");
-					}
+        $race = new BBLM_CPT_Race;
 
+        echo '<h3 class="bblm-table-caption">' . __( 'Positions available for Race', 'bblm' ) . '</h3>';
+        $race->display_race_positions( $race_id );
 
-					print("<h3>Teams belonging to this Race</h3>\n");
-					$teamsql = 'SELECT T.WPID FROM '.$wpdb->prefix.'team T WHERE T.t_show = 1 AND T.r_id = '.$race_id.' ORDER by T.t_name ASC';
-					if ($teams = $wpdb->get_results($teamsql)) {
-						print("<ul>\n");
-						foreach ($teams as $td) {
-							print("<li><a href=\"" . get_post_permalink( $td->WPID ) . "\" title=\"View more details of this team\">" . esc_html( get_the_title( $td->WPID ) ) . "</a></li>\n");
-						}
-						print("</ul>\n");
-					}
-					else {
-						print("	<div class=\"bblm_info\">\n		<p>There are currently no teams representing this Race.</p>\n	</div>.\n");
-					}
-				}
-				else {
-					print("	<div class=\"bblm_info\">\n		<p>There are currently no details set up for this Race.</p>\n </div>\n");
-				}
+        echo '<h3 class="bblm-table-caption">' . __( 'Star Players available for Race', 'bblm' ) . '</h3>';
+        $race->display_stars_available( $race_id );
+
+        echo '<h3 class="bblm-table-caption">' . __( 'Teams belonging to this Race', 'bblm' ) . '</h3>';
+        $race->display_teams_representing( $race_id );
+
 
 ?>
 </div><!-- .entry-content -->
